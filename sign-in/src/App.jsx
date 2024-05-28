a40f0ebc67cb490cbc18f28a9a6302fa2724e148
theleafsyn <ct060331@actvn.edu.vn>
2024-05-29 00:22:11 +0700

    P2

--- sign-in/src/App.jsx
+++ sign-in/src/App.jsx
@@ -3,25 +3,6 @@ import React from "react";
 import Signup from "./component/sign-up";
 import Login from "./component/login";
 
-function openTab(evt, tabName) {
-  var i, tabcontent, tablinks;
-
-  // Hide all tab content
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
-
-  // Remove the "active" class from all tab links
-  tablinks = document.getElementsByClassName("tablink");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].classList.remove("active");
-  }
-
-  // Show the selected tab content and add the "active" class to the clicked tab link
-  document.getElementById(tabName).style.display = "block";
-  evt.currentTarget.classList.add("active");
-}
 
 function App() {
   return (
---
+++ sign-in/src/component/home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <div class="slide_show">
+        
+      </div>
+    </div>
+  );
+}
