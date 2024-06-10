776dc793ae5f6c4f4dae25945c2de7fefc127851
Hung <hung110195@gmail.com>
2024-06-10 13:04:03 +0700

    a

---
+++ CenterManagement/CenterManagement/wwwroot/views/student/ListClassesStudents/Controller.js
@@ -0,0 +1,276 @@
+﻿var ctxfolder = "/views/student/ListClassesStudents";
+
+var app = angular.module("App_ESEIM", ["ngRoute", "ngResource", "ui.bootstrap", "datatables"]);
+
+//var app = angular.module('App_ESEIM', ["ui.bootstrap", "ngRoute", "ngCookies", "ngValidate", "datatables", "datatables.bootstrap", "pascalprecht.translate", "ngJsTree", "treeGrid", 'datatables.colvis', "ui.bootstrap.contextMenu", 'datatables.colreorder', 'angular-confirm', 'ui.select', 'ui.tinymce', 'dynamicNumber', 'ngTagsInput']);
+
+app.controller("Ctrl_ESEIM", function ($scope, $rootScope) {
+
+
+
+
+
+    $rootScope.studentData = {
+        "id":23,
+        "name": "3.1",
+        "year": 2024,
+        "grade": 3,
+        "lessons": [
+            {
+                "date": "2024-05-29",
+                "topic": "Introduction to Algebra",
+                "teacherId": "66640f7ccf0f68d1c98dad85"
+            },
+            {
+                "date": "2024-05-30",
+                "topic": "Linear Equations",
+                "teacherId": "66640f7ccf0f68d1c98dad85"
+            },
+            {
+                "date": "2024-06-01",
+                "topic": "Statistics",
+                "teacherId": "66640f7ccf0f68d1c98dad85"
+            }
+        ]
+    };
+
+
+
+
+});
+
+app.config(function ($routeProvider) {
+    $routeProvider
+        .when("/", {
+            templateUrl: ctxfolder + '/index.html',
+            controller: "index"
+        })
+        .otherwise({
+            redirectTo: "/"
+        });
+
+
+});
+
+
+app.controller('index', function ($scope, $compile, $rootScope, $http, $uibModal, DTOptionsBuilder, DTColumnBuilder) {
+
+
+
+    
+
+
+
+
+
+    vm = $scope;
+    vm.dtOptions = DTOptionsBuilder.newOptions()
+
+
+        //.withOption('ajax', {
+        //    url: 'your-api-endpoint', // URL của API của bạn
+        //    type: 'GET', // Loại yêu cầu (GET hoặc POST)
+        //    dataSrc: 'data' // Tên thuộc tính trong phản hồi API chứa dữ liệu
+        //})
+        //.withDataProp('data') // Cách cũ để chỉ định thuộc tính dữ liệu
+
+
+        
+        .withPaginationType('full_numbers')
+        .withDisplayLength(9)
+        .withOption('autoWidth', false)
+        .withOption('processing', true)
+        .withOption('lengthChange', false)
+        .withOption('searching', false)
+        .withOption('scrollX', false)
+        .withOption('pageLength', 10)
+        .withOption('scrollCollapse', true)
+        //tự điều chỉnh độ rộng của cột khớp màn hình
+        .withLanguage({
+            "info": "_END_ / _TOTAL_ mục",
+            "paginate": {
+                "first": '<<',
+                "last": '>>',
+                "next": 'tiếp',
+                "previous": 'trước'
+            },
+            "lengthMenu": "Hiển thị _MENU_ mục",
+            "search": "Tìm kiếm:",
+            "infoEmpty": "Không có dữ liệu",
+            "infoFiltered": "(lọc từ _MAX_ mục)",
+            "zeroRecords": "Không tìm thấy dữ liệu"
+        })
+
+
+        
+        /*.withOption('scrollX', false)*/
+        /*  .withOption('serverSide', true)*/
+        .withOption('columnDefs', [
+            { targets: 0, visible: false },  // Ẩn cột đầu tiên          
+        ])
+        .withOption('createdRow', function (row, data, dataIndex) {
+            $compile(angular.element(row).contents())($scope);
+        });
+
+    vm.dtColumns = [
+        DTColumnBuilder.newColumn('id').withTitle('ID').renderWith(function (data, type) {
+            return data;
+        }),
+        DTColumnBuilder.newColumn('name').withTitle('Tên').renderWith(function (data, type) {
+            return data;
+        }),
+        DTColumnBuilder.newColumn('year').withTitle('Năm học').renderWith(function (data, type) {
+            return data;
+        }),
+        DTColumnBuilder.newColumn('grade').withTitle('Lớp').renderWith(function (data, type) {
+            return data;
+        }),
+        DTColumnBuilder.newColumn('Absent').withTitle('Đã nghỉ').renderWith(function (data, type) {
+            return data;
+        }),    
+        DTColumnBuilder.newColumn('action').notSortable().withTitle('Các buổi học').renderWith(function (data, type, full, meta) {
+            return '<button title="Chi tiết" ng-click="detail(' + full.Id + ')" style="width: 25px;pointer-events: auto !important; height: 25px; padding: 0px;-webkit-box-shadow: 0 2px 5px 0 rgb(0 3 6 / 97%);border-radius: 50%;" class="btn btn-icon-only btn-circle btn-outline-button-icon"><i class="fa-solid fa-eye"></i></button>';
+
+
+        })
+    ];
+
+    vm.dtInstance = {};
+
+    vm.dtOptions.data = [
+        { Id: 1, Class: '3.1A', Year: '01/01/2000', Absent: 2, Grade: 'Grade 1', Lession: {date:'ágfufew',topic:'hjewegh'} },
+        { Id: 2, Class: '25A', Year: '01/02/2000', Absent: 4, Grade: 'Grade 2', Lession: { date: 'ágfufew', topic: 'hjewegh' } },
+        { Id: 3, Class: '40A', Year: '01/03/2000', Absent: 7, Grade: 'Grade 3', Lession: { date: 'ágfufew', topic: 'hjewegh' } }
+    ];
+    /*vm.dtOptions.data = $rootScope.studentData;*/
+
+
+    //$scope.response = {};
+    // $http.get('http://localhost:3000/api/v1/student')
+    //    .then(function (response) {
+    //        $scope.response = response.data;
+    //        vm.dtOptions.data = $scope.response.metadata;
+    //    })
+    //    .catch(function (error) {
+    //        console.error('Error:', error);
+    //    });
+
+
+    $scope.detail = function (Id) {
+        console.log('Opening detail modal for student with id:', Id);
+        var modalInstance = $uibModal.open({
+            animation: true,
+            templateUrl: ctxfolder + '/detail.html',
+            controller: 'detail',
+            backdrop: 'static',
+            size: 'lg',
+
+            resolve: {
+                studentId: function () {
+                    return Id;
+                }
+            }
+        });
+        modalInstance.opened.then(function () {
+            $('.modal').css({
+                'display': 'block',
+                'visibility': 'visible',
+                'opacity': '1',
+                'top': '30vh',
+                'left': '23%'
+            });
+        });
+        modalInstance.rendered.then(function () {
+            var modalElement = angular.element(document.querySelector('.modal'));
+            modalElement.addClass('modal-lg');
+        });
+
+        modalInstance.result.then(function () {
+            $scope.reload();
+        }, function () {
+            console.log('Modal dismissed at: ' + new Date());
+        });
+    };
+
+ 
+
+
+
+});
+
+
+
+app.controller('detail', function ($scope, $uibModalInstance, $rootScope,studentId, $compile, $uibModal, DTOptionsBuilder, DTColumnBuilder) {
+
+    $scope.ok = function () {
+        $uibModalInstance.close();
+    };
+
+    $scope.cancel = function () {
+        $uibModalInstance.dismiss('cancel');
+    };
+
+
+
+
+    vm = $scope;
+    vm.dtOptions = DTOptionsBuilder.newOptions()
+        .withPaginationType('full_numbers')
+        .withDisplayLength(9)
+        .withOption('autoWidth', false)
+        .withOption('processing', true)
+        .withOption('lengthChange', false)
+        .withOption('searching', false)
+        .withOption('scrollX', false)
+        .withOption('pageLength', 10)
+        .withOption('scrollCollapse', true)
+        //tự điều chỉnh độ rộng của cột khớp màn hình
+        .withLanguage({
+            "info": "_END_ / _TOTAL_ mục",
+            "paginate": {
+                "first": '<<',
+                "last": '>>',
+                "next": 'tiếp',
+                "previous": 'trước'
+            },
+            "lengthMenu": "Hiển thị _MENU_ mục",
+            "search": "Tìm kiếm:",
+            "infoEmpty": "Không có dữ liệu",
+            "infoFiltered": "(lọc từ _MAX_ mục)",
+            "zeroRecords": "Không tìm thấy dữ liệu"
+        })
+
+        /*.withOption('scrollX', false)*/
+        /*  .withOption('serverSide', true)*/
+        .withOption('columnDefs', [
+            { targets: 0, visible: false },  // Ẩn cột đầu tiên          
+        ])
+        .withOption('createdRow', function (row, data, dataIndex) {
+            $compile(angular.element(row).contents())($scope);
+        });
+
+    vm.dtColumns = [
+        DTColumnBuilder.newColumn('Id').withTitle('ID').renderWith(function (data, type) {
+            return data;
+        }),
+        DTColumnBuilder.newColumn('date').withTitle('Thời gian').renderWith(function (data, type) {
+            return data;
+        }),
+        DTColumnBuilder.newColumn('topic').withTitle('Bài giảng').renderWith(function (data, type) {
+            return data;
+        }),
+        DTColumnBuilder.newColumn('teacherID').withTitle('Giáo viên').renderWith(function (data, type) {
+            return data;
+        }),
+
+    ];
+
+    vm.dtInstance = {};
+    vm.dtOptions.data = $rootScope.studentData.lessons;
+
+});
+
+
+
+
+
