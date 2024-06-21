c387f3cd0089c386ec00f70f39e2696b2c87b45d
theleafsyn <ct060331@actvn.edu.vn>
2024-06-21 17:56:02 +0700

    p5

--- CenterManagement/obj/Debug/net6.0/CenterManagement.AssemblyInfo.cs
+++ CenterManagement/obj/Debug/net6.0/CenterManagement.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CenterManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6d818247a2ca06fd52b04ae78144fa1a196dff7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+199025df2fe8301a02c03f62533103475ac2a029")]
 [assembly: System.Reflection.AssemblyProductAttribute("CenterManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CenterManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
