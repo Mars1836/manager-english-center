977fbe7f37949639e7379157713fbd24a71642cf
Mars1836 <hauhpll123@gmail.com>
2024-06-28 14:37:00 +0700

    a

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,6 @@ require("dotenv").config();
 const server = app.listen(process.env.DEV_PORT, () => {
   console.log("server running in port : " + process.env.DEV_PORT || 3000);
 });
-
 process.on("SIGINT", () => {
   server.close(() => {
     console.log("Exit server express");
