ba056875b70fa6308a921542826e50a6ac0be8e6
Mars1836 <hauhpll123@gmail.com>
2024-06-29 16:33:07 +0700

    a

---
+++ backend/src/models/repo/class.repo.js
@@ -0,0 +1,31 @@
+const classModel = require("../class.model");
+const lessonModel = require("../lesson.model");
+const _ = require("lodash");
+
+class ClassRepo {
+  static async findByStudent({ studentId }) {
+    const classes = await classModel
+      .find({ students: studentId })
+      .select(" -__v -createdAt -updatedAt -id")
+      .populate({
+        path: "lesson",
+        model: lessonModel,
+        select: "topic teacherId isFinished attendance",
+      })
+      .lean();
+    const rs = classes.map((item) => {
+      item.totalLesson = item?.lesson.length || 0;
+      item.registeredStudents = item?.students.length || 0;
+      item.statusClasses = item.status;
+      item.statusRegister =
+        item.students.findIndex((id) => {
+          return id.toString() === studentId;
+        }) > -1
+          ? true
+          : false;
+      return _.omit(item, ["lesson", "status"]);
+    });
+    return rs;
+  }
+}
+module.exports = ClassRepo;
--- backend/src/services/lesson.service.js
+++ backend/src/services/lesson.service.js
@@ -1,21 +1,21 @@
 const adminModel = require("../models/admin.model");
 const lessonModel = require("../models/lesson.model");
-const StudentService = require("./student.service");
+const StudentRepo = require("../models/repo/student.repo");
 const TeacherService = require("./teacher.service");
+
 class LessonService {
   static async checkConflict({ studentId, classId }) {
-    const a = await StudentService.getSchedule({ studentId });
+    const a = await StudentRepo.getSchedule({ studentId });
     const b = await lessonModel.find({ classId }).lean();
     return check(a, b);
   }
   static async checkConflictStudentEnroll({ studentId, classId }) {
-    const a = await StudentService.getSchedule({ studentId });
+    const a = await StudentRepo.getSchedule({ studentId });
     const b = await lessonModel.find({ classId }).lean();
     return check(a, b);
   }
   static async checkConflictAddTeacherToLesson({ teacherId, lesson }) {
     const a = await TeacherService.getSchedule({ studentId });
-
     return check(a, [lesson]);
   }
 }
@@ -40,7 +40,6 @@ function check(a1, a2) {
 function isTimeOverlap(start1, end1, start2, end2) {
   const a =
     new Date(start1) < new Date(end2) && new Date(start2) < new Date(end1);
-  console.log(a);
   return a;
 }
 module.exports = LessonService;
