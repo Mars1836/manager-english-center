ae74b2ce3e4cd34b25226cb0ae9896c5903711ac
Mars1836 <hauhpll123@gmail.com>
2024-07-01 10:03:44 +0700

    a

--- backend/src/models/repo/class.repo.js
+++ backend/src/models/repo/class.repo.js
@@ -3,16 +3,30 @@ const lessonModel = require("../lesson.model");
 const _ = require("lodash");
 
 class ClassRepo {
-  static async findByStudent({ studentId }) {
-    const classes = await classModel
-      .find()
-      .select(" -__v -createdAt -updatedAt -id")
-      .populate({
-        path: "lesson",
-        model: lessonModel,
-        select: "topic teacherId isFinished attendance",
-      })
-      .lean();
+  static async findByStudent({ studentId, map = false }) {
+    let classes;
+    if (map === true) {
+      classes = await classModel
+        .find({ students: studentId })
+        .select(" -__v -createdAt -updatedAt -id")
+        .populate({
+          path: "lesson",
+          model: lessonModel,
+          select: "topic teacherId isFinished attendance",
+        })
+        .lean();
+    } else {
+      classes = await classModel
+        .find({})
+        .select(" -__v -createdAt -updatedAt -id")
+        .populate({
+          path: "lesson",
+          model: lessonModel,
+          select: "topic teacherId isFinished attendance",
+        })
+        .lean();
+    }
+
     const rs = classes.map((item) => {
       item.totalLesson = item?.lesson.length || 0;
       item.registeredStudents = item?.students.length || 0;
